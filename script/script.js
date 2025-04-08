8fe709f5592d7c47fa16cb1731841b984738ada1
ModoLau <chloe2222.cl@gmail.com>
2025-04-08 16:11:53 +0800

    1.5

--- script/script.js
+++ script/script.js
@@ -6,3 +6,22 @@ function showMenu(){
 function hideMenu(){
     navLink.style.right = "-200px";
 }
+
+const typewriter = document.getElementById('typewriter');
+const fullText = "I am a School Head IT Prefect";
+let typing = false;
+
+function typeText() {
+    let index = 0;
+    typing = true;
+
+    const interval = setInterval(() => {
+        if (index < fullText.length) {
+            typewriter.textContent += fullText.charAt(index);
+            index++;
+        } else {
+            clearInterval(interval);
+            typing = false;
+        }
+    }, 100); // Adjust typing speed
+}
