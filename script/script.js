2860c79a0d5431948f25c0867265ada645fe1679
Modo Lau <chloe2222.cl@gmail.com>
2025-04-10 23:11:43 +0800

    2.2

--- script/script.js
+++ script/script.js
@@ -69,4 +69,14 @@ document.addEventListener("DOMContentLoaded", () => {
     window.addEventListener('scroll', onScroll);
 });
 
+let cards = document.querySelectorAll('.card-box-patrol, .card-box-ctrm, .card-box-stage');
 
+cards.forEach(card => {
+    card.onmousemove = (e) => {
+        let x = e.pageX - card.offsetLeft;
+        let y = e.pageY - card.offsetTop;
+
+        card.style.setProperty("--x", x + "px");
+        card.style.setProperty("--y", y + "px");
+    };
+});
